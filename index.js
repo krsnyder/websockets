abf4640402cb231ee9b8429af2a803e689281d42
Kirk <Kirk.Robert.Snyder@gmail.com>
2022-01-04 14:08:03 -0500

    Cleaning up backend

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const server = require('./server');
+const socket = require('socket.io');
 
 const app = server.listen(8000, () => {
   console.log("run it")
+});
+
+const io = socket(app);
+
+io.on('connection', socket => {
+  console.log('Made connection: ' + socket)
 });
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const server = express();
 
-server.use(express.static('public'));
-
 // server.use("*", (req, res) => {
 //   console.log("Catching all requests")
 // });
+
+server.use(express.static('public'));
 
 module.exports = server;
